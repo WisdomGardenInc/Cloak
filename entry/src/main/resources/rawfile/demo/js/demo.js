e5c596b63b4fea388d473506272f12e3e722e415
liupeixin <fengyi.mail@gmail.com>
2025-02-13 18:10:21 +0800

    demo

--- entry/src/main/resources/rawfile/demo/js/demo.js
+++ entry/src/main/resources/rawfile/demo/js/demo.js
@@ -1,3 +1,14 @@
-window.onload = function() {
+window.onload = function () {
     document.querySelector("div.demo").innerText = window.location.href + "\n" + window.navigator.userAgent;
+}
+
+
+const onAlert = () => {
+    const result = alert("alert content")
+    console.log(result)
+}
+
+const onConfirm = () => {
+    const result = confirm("confirm")
+    console.log(result)
 }
