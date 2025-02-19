c54f37826c602da976d4d0d1f8e77797166a798b
liupeixin <fengyi.mail@gmail.com>
2025-02-19 17:26:46 +0800

    refactor code

--- entry/src/main/resources/rawfile/CloakClient.js
+++ entry/src/main/resources/rawfile/CloakClient.js
@@ -1,8 +1,8 @@
 (function () {
-    const clockPlugins = Object.keys(window).filter(key => key.startsWith('_cloak_plugin_'))
+    const cloakPlugins = Object.keys(window).filter(key => key.startsWith('_cloak_plugin_'))
     Cloak.plugins = {};
     Cloak.metadata = Cloak.getMetadata();
-    clockPlugins.forEach(pluginName => {
+    cloakPlugins.forEach(pluginName => {
         const newName = pluginName.substr(14);
         Cloak.plugins[newName] = window[pluginName];
         Cloak.plugins[newName].metadata = Cloak.plugins[newName].getMetadata();
