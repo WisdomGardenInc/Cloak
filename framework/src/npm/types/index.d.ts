6ff3039ae56391ec162f9e9d4862ab027f942ac9
yaoxiangyi <yao1909@163.com>
2026-05-20 11:29:06 +0800

    fix: fix types

--- framework/src/npm/types/index.d.ts
+++ framework/src/npm/types/index.d.ts
@@ -5,12 +5,17 @@ import type { MessagePort } from "./base";
 import type { PluginMetadata } from "./base";
 import type { CloakPlugins } from "./plugins";
 
+export interface CloakMetadata extends PluginMetadata {
+  platform: "HarmonyOS";
+}
+
 export interface Cloak {
   readonly channel: MessagePort;
   readonly plugins: CloakPlugins;
-  readonly metadata: PluginMetadata;
-  readonly platform: "HarmonyOS";
-  getMetadata(): PluginMetadata;
+  readonly metadata: CloakMetadata;
+  readonly name: "Cloak";
+  readonly registered: boolean;
+  getMetadata(): CloakMetadata;
 }
 
 export const Cloak: Cloak;
